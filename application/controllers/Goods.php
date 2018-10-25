49b72fe24b0cee3da3424b4cd5395c9243c58293
Zhangyayaa <1029390260@qq.com>
2018-10-25 10:57:22 +0800

    1057

--- application/controllers/Goods.php
+++ application/controllers/Goods.php
@@ -185,10 +185,10 @@ class Goods extends AdminController {
 			}else{
 				$querys->equalTo('objectId', $pid);
 				$parent = $querys->find();
-				// $pname = $parent->get('title');
+				$pname = $parent->get('title');
 
-				var_dump($parent);
-				// $value->set('pname', $pname);
+				var_dump($pname);
+				$value->set('pname', $pname);
 			}
 		}
 		$data['result'] = $result;
