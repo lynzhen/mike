1184c635bb99461fcdd56687dca3019fa395027f
Zhangyayaa <1029390260@qq.com>
2018-10-29 12:57:02 +0800

    1256

--- application/views/goods/edit.php
+++ application/views/goods/edit.php
@@ -54,10 +54,77 @@
                     </select>
                   </div>
                 </div>
-                <div class="form-group">
+                <!-- <div class="form-group">
                   <label for="price" class="col-sm-2 control-label">价格</label>
                   <div class="col-sm-8">
                     <input type="number" class="form-control" name="price" id="price" placeholder="0.0" value="<?=$goods->get('price')?>">
+                  </div>
+                </div> -->
+                <div class="form-group">
+                  <label for="singleCode" class="col-sm-2 control-label">单瓶条码</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="singleCode" id="singleCode" placeholder="单瓶条码" value="<?=$goods->get('singleCode')?>">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="suppliers" class="col-sm-2 control-label">供货商名称</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="suppliers" id="suppliers" placeholder="供货商名称" value="<?=$goods->get('suppliers')?>">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="price" class="col-sm-2 control-label">进货价</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="price" id="price" placeholder="进货价" value="<?=$goods->get('price')?>">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="boxnumber" class="col-sm-2 control-label">箱入数</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="boxnumber" id="boxnumber" placeholder="箱入数" value="<?=$goods->get('boxnumber')?>">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="FCL" class="col-sm-2 control-label">必须整箱订货</label>
+                  <div class="col-sm-8">
+                    <div class="btn-group" id="FCL" data-toggle="buttons">
+                      <label class="btn btn-default <?=$goods->get('FCL') == true ? 'active' : ''?>">
+                        <input type="radio" name="FCL" value="1" id="option1" autocomplete="off" checked> 是
+                      </label>
+                      <label class="btn btn-default <?=$goods->get('FCL') == false ? 'active' : ''?>">
+                        <input type="radio" name="FCL" value="0" id="option3" autocomplete="off"> 否
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="size" class="col-sm-2 control-label">箱单位</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="size" id="size" placeholder="箱单位" value="<?=$goods->get('size')?>">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="boxprice" class="col-sm-2 control-label">箱价格</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="boxprice" id="boxprice" placeholder="箱价格" value="<?=$goods->get('boxprice')?>">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="singleTP" class="col-sm-2 control-label">单瓶商品规格</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="singleTP" id="singleTP" placeholder="单瓶商品规格" value="<?=$goods->get('singleTP')?>">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="singleSize" class="col-sm-2 control-label">单瓶单位</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="singleSize" id="singleSize" placeholder="单瓶单位" value="<?=$goods->get('singleSize')?>">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="barcode" class="col-sm-2 control-label">条码</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="barcode" id="barcode" placeholder="条码" value="<?=$goods->get('barcode')?>">
                   </div>
                 </div>
                 <div class="form-group">
@@ -220,7 +287,15 @@
           objectId: $('#objectId').val(),
           title: $('#title').val(),
           category: $('#category').val(),
+          singleCode: $('#singleCode').val(),
+          suppliers: $('#suppliers').val(),
           price: $('#price').val(),
+          boxnumber: $('#boxnumber').val(),
+          FCL: $('#FCL .active input').val(),
+          size: $('#size').val(),
+          singleTP: $('#singleTP').val(),
+          singleSize: $('#singleSize').val(),
+          barcode: $('#barcode').val(),
           isNew: $('#isNew .active input').val(),
           isHot: $('#isHot .active input').val(),
           images: $('#images').val(),
