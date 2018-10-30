425baa632cc718236719685b5676934f920513a1
Zhangyayaa <1029390260@qq.com>
2018-10-30 08:37:46 +0800

    837

--- application/models/Category_model.php
+++ application/models/Category_model.php
@@ -6,31 +6,22 @@ class Category_model extends CI_Model {
 	// 获取全部分类
 	public function findAll() {
 		// 1. 查询所有顶级分类
-		//
 		$query = new Query("Category");
-		$query->equalTo('IsParent', true);
+		// $query->equalTo('IsParent', true);
 		$categoris = $query->find();
 		//一级分类
 			var_dump($categoris);
+			// die();
 		// 2. sub  
-		// if(!empty($categoris)){
-		foreach ($categoris as $category) {	
-			 $parentId = $category->get('NCategory_OnlyId');
-			// var_dump($parent);Category
-			$newquerys=new Query("Category");
-			$newquerys->equalTo('NparentId', $parentId);
-			$children = $newquerys->find();		
-			echo '所有的二级分类';
-			var_dump($parentId);
-		
-			var_dump($children);
+		foreach ($categoris as $category) {				
+			$query->equalTo('parent', $category);
+			$children = $query->find();
 			// 不必使用转数组再动态添加成员属性，$category = $category->toJSON();object同样可以实现操作
 			$category->children = $children;
 			$result[] = $category;
 
 		}
 		return $result;
-		// }
 	
 	}
 
