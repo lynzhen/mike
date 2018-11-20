760780101c8252b5a6ddc0623bceebd2bd481b66
Zhangyayaa <1029390260@qq.com>
2018-11-20 09:38:26 +0800

    938

--- application/views/goods/flist.php
+++ application/views/goods/flist.php
@@ -102,6 +102,7 @@
 				'showList',				
 				function (response) {
 					console.log(response);
+					console.log(JSON.parse(response))
 					// sweetAlert("提示", response.message, "success");
 				}  
 			);
