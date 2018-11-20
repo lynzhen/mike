532fdf8d54d357f752c789d4a4a0b89b45c8a4a2
Zhangyayaa <1029390260@qq.com>
2018-11-20 16:02:01 +0800

    1601

--- application/views/goods/index.php
+++ application/views/goods/index.php
@@ -211,7 +211,7 @@
 			function (response) {
 				console.log(response);
 				console.log(typeof(response));
-				console.log(JSON.stringify(response));return;
+				console.log(eval('(' + response + ')'));return;
 				// var data = JSON.parse(response);
 				var data = response;
 				$(".sweet-overlay,.sweet-alert").hide();
