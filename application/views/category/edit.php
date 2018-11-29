7ea1a835e9eeefc8d6a519a3de3ef31bc888f47b
Zhangyayaa <1029390260@qq.com>
2018-11-29 19:54:50 +0800

    1954

--- application/views/category/edit.php
+++ application/views/category/edit.php
@@ -71,7 +71,7 @@
                   <div class="col-sm-8">
                     <?php
                       if ($categorys->get('avatar') != null) {
-                        echo '<img class="avatar" src="' . $categorys->get('flno').'">';
+                        echo '<img class="avatar" src="' . $categorys->get('avatar').'">';
                       }
                     ?>
                     <input type="file" name="avatar" id="avatar" value="">
@@ -83,7 +83,7 @@
                   <div class="col-sm-8">
                     <?php
                       if ($categorys->get('banner') != null) {
-                        echo '<img class="avatar" src="' . $categorys->get('flno').'">';
+                        echo '<img class="avatar" src="' . $categorys->get('banner').'">';
                       }
                     ?>
                     <input type="file" name="banner" id="banner" value="">
